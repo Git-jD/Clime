c81ea6088ea943ddff1206421f7d99cd17e18e48
Jyoti <kaju47901s@gmail.com>
2021-02-20 02:38:04 +0530

    AssetImage

--- clime/lib/main.dart
+++ clime/lib/main.dart
@@ -20,7 +20,7 @@ class Home extends StatelessWidget {
       ),
       body: Center(
         child: Image(
-          image: NetworkImage('https://images.unsplash.com/photo-1502581827181-9cf3c3ee0106?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=721&q=80'),
+          image: AssetImage('images/Space.jpg'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
